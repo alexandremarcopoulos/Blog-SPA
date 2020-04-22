5525cfe25629ec9bbca9730713c1291a495db220
alexandre marcopoulos <alexandremarcopoulos28@gmail.com>
2020-04-21 21:29:08 -0300

    Criação de conteudo para a pagina home, navbar e footer customizado, novas paginas como produto e reserva

--- blog-SPA/src/app/app.module.ts
+++ blog-SPA/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReservaComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
