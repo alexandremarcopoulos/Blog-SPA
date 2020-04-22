a489784b13cefb4b535ba69c5bc8573594bfea53
alexandre marcopoulos <alexandremarcopoulos28@gmail.com>
2020-04-21 22:18:24 -0300

    Rotas criadas

--- blog-SPA/src/app/app-routing.module.ts
+++ blog-SPA/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { ReservaComponent } from './reserva/reserva.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'produto', component: ProdutoComponent },
+  { path: 'reserva', component: ReservaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
